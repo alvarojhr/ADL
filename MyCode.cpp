4a5426b4a8dbc6f33f9d1f7d914bd74b3466c50c
Docente UPB <docente@bga.upb>
2023-10-02 07:04:42 -0500

    Adding name

--- MyCode.cpp
+++ MyCode.cpp
@@ -5,8 +5,12 @@ using namespace std;
 
 int main()
 {
+    string name;
     cout<<"Hola mundo";
     cout<<"Bienvenidos a Analisis!";
+
+    cout << "Type your name: ";
+    getline(cin, name);
 
     return 0;
 }
